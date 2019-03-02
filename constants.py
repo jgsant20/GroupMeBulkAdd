a4a2a5441105164f54882ee6d86633728888d278
Justin Santos <jgsant20@gmail.com>
2019-03-02 06:37:32 -0600

    Replace ALL tokens and numbers with dummies.

--- constants.py
+++ constants.py
@@ -10,23 +10,18 @@ QUERY = GROUPME_API_URL + "/groups?omit='memberships'&token=%s" % ACCESS_TOKEN
 
 # ==============================================================
 # Constants below used for quick access if group_id and rosters are already known.
+# FOR QUICK_RUN
 
 # GroupMe group ID
 GROUP_ID = ''
 
 # CSV file that contains all numbers
-ROSTER = 'csv.txt'
+ROSTER = 'csv/test.txt'
+
+
+# ==============================================================
+# Constants below used for quick access if group_id and rosters are already known.
+# FOR BULK_RUN
 
 # Contains list of tuples(group_id, roster)
-
-"""
-DATA = [(47750320, 'csv/Setup.csv'),
-        (47750380, 'csv/First Shift.csv'),
-        (47750384, 'csv/Ongoing.csv'),
-        (47750391, 'csv/I.Ongoing.csv'),
-        (47750395, 'csv/Last Shift.csv'),
-        (47750400, 'csv/Cleanup.csv'),
-        ]
-"""
-
-DATA = [(47902602, 'csv/test.csv')]
+DATA = [(00000000, 'csv/test.csv')]
